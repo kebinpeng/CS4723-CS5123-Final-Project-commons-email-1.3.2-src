21ea480148d67b0e137f17b3cade218e45d15bac
pkb <15718775@qq.com>
2020-11-19 09:31:49 -0600

    add test files

---
+++ src/test/java/org/apache/commons/mail/EmailTest2.java
@@ -0,0 +1,23 @@
+package org.apache.commons.mail;
+
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+import junit.framework.TestCase;
+
+public class TemplateTest extends TestCase{
+	Email testEmail;
+	public void setUp() {
+		testEmail = new SimpleEmail();
+	}
+	@Test
+	public void test_addReplyTo() throws EmailException {
+		System.out.println("This is a message from the HKY test.");
+		testEmail.addReplyTo("a@b.com","tom");
+		assertEquals("tom <a@b.com>", testEmail.getReplyToAddresses().get(0).toString());
+	}
+
+}
+
+        
